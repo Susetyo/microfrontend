e49b5df630259033afda9c4495f039e2e2686e8d
susetyo <susetyamulia@gmail.com>
2021-06-29 20:25:38 +0700

    add dashboard suppapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,15 @@
-import React, {lazy, Suspense, useState} from "react";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, {lazy, Suspense, useState, useEffect} from "react";
+import {Router, Route, Switch, Redirect} from 'react-router-dom';
 import Progress from "./components/Progress";
 import Header from "./components/Header";
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
+import { createBrowserHistory } from "history";
 
 const MarketingAppLazy = lazy(()=> import('./components/MarketingApp'));
 const AuthAppLazy = lazy(()=>import('./components/AuthApp'));
+const DashboardAppLazy = lazy(()=>import('./components/DashboardApp'));
 
-
+const history = createBrowserHistory();
 
 const generateClassName = createGenerateClassName({
     productionPrefix:'co'
@@ -15,21 +17,32 @@ const generateClassName = createGenerateClassName({
 
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(()=>{
+        if(isSignedIn){
+            history.push('/dashboard')
+        }
+    },[isSignedIn])
+
     return(
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
-                <div>
-                    <Header isSignedIn={isSignedIn} onSignOut={()=> setIsSignedIn(false)} />
-                    <Suspense fallback={<Progress />}>
-                        <Switch>
-                            <Route path="/auth">
-                                <AuthAppLazy onSignIn={()=> setIsSignedIn(true)} />
-                            </Route>
-                            <Route path="/" component={MarketingAppLazy} />
-                        </Switch>
-                    </Suspense>
-                </div>
-            </BrowserRouter>
-        </StylesProvider>
+        <Router history={history}>
+            <StylesProvider generateClassName={generateClassName}>
+                    <div>
+                        <Header isSignedIn={isSignedIn} onSignOut={()=> setIsSignedIn(false)} />
+                        <Suspense fallback={<Progress />}>
+                            <Switch>
+                                <Route path="/auth">
+                                    <AuthAppLazy onSignIn={()=> setIsSignedIn(true)} />
+                                </Route>
+                                <Route path="/dashboard">
+                                    {!isSignedIn &&  <Redirect to="/" />}
+                                    <DashboardAppLazy />
+                                </Route>
+                                <Route path="/" component={MarketingAppLazy} />
+                            </Switch>
+                        </Suspense>
+                    </div>
+            </StylesProvider>
+        </Router>
     )
 }
